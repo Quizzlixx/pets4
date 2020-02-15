6a9058c6e25bcf9f15a975c7e80e4ff2ac5d32e1
Kerrie Low <klow4@mail.greenriver.edu>
2020-02-15 00:00:30 -0500

    Modified index to call controller class. Created a method for each route. Updated composer

--- index.php
+++ index.php
@@ -27,6 +27,9 @@ error_reporting(E_ALL);
 //instantiate F3
 $f3 = Base::instance();
 
+// instantiate controller object
+$controller = new Pets4Controller($f3);
+
 //Set dubug level
 $f3->set('DEBUG', 3);
 
@@ -35,109 +38,26 @@ $f3->set('colors', array("pink", "green", "blue"));
 
 //DEFINE A DEFAULT ROUTE
 $f3->route('GET /', function () {
-    echo "<h1>Hello Pets!</h2>";
-
-    echo "<a href='animal'>Order a Pet</a>";
+    $GLOBALS['controller']->home();
 });
 
+// pick animal type
 $f3->route('GET|POST /animal', function ($f3) {
-//    var_dump($_POST);
-
-    unset($_SESSION['animal']);
-
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-        // collect POST variable
-        $animal = $_POST['animal'];
-
-        // add data to hive
-        $f3->set('animal', $animal);
-
-        if (isset($animal) && validString($animal)) {
-
-            // set session data
-            $_SESSION['animal'] = $animal;
-
-            // instantiate pet object
-            makePet($animal);
-
-            // go to color form
-            $f3->reroute('/color');
-        } else {
-            $f3->set("errors['animal']", "please enter an animal.");
-        }
-    }
-
-    $view = new Template();
-    echo $view->render('views/typeForm.html');
+    $GLOBALS['controller']->animal($f3);
 });
 
+// pick animal color
 $f3->route('GET|POST /color', function ($f3) {
-//    var_dump($_POST);
-
-    unset($_SESSION['color']);
-
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-        // collect POST variable
-        $color = $_POST['color'];
-
-        // send to hive
-        $f3->set('selectedColor', $color);
-
-        if (isset($color) && validColor($color)) {
-            // set session variable
-            $_SESSION['color'] = $color;
-
-            // set pet1 objects color
-            $_SESSION['pet1']->setColor($color);
-
-            // send to name form
-            $f3->reroute('/name');
-        } else {
-            $f3->set("errors['color']", "please enter a valid color.");
-        }
-    }
-
-    $view = new Template();
-    echo $view->render('views/colorForm.html');
+    $GLOBALS['controller']->color($f3);
 });
 
+// pick animal name
 $f3->route('GET|POST /name', function ($f3) {
-//    var_dump($_POST);
-
-    unset($_SESSION['name']);
-
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-        // collect POST variable
-        $name = $_POST['name'];
-        // send to hive
-        $f3->set('name', $name);
-
-        if (isset($name) && validString($name)) {
-            // set session variable
-            $_SESSION['name'] = $name;
-
-            // set object name
-            $_SESSION['pet1']->setName($name);
-
-            // reroute to result page
-            $f3->reroute('/results');
-        } else {
-            $f3->set("errors['name']", "please enter a name.");
-        }
-    }
-
-    $view = new Template();
-    echo $view->render('views/nameForm.html');
+    $GLOBALS['controller']->name($f3);
 });
 
-$f3->route('GET|POST /results', function ($f3) {
-//    var_dump($_SESSION);
-
-    $view = new Template();
-    echo $view->render('views/results.html');
+$f3->route('GET|POST /results', function () {
+    $GLOBALS['controller']->results();
 });
 
 // run f3
