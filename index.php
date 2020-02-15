5f472be2da6d5425f9e4d852ee0bb477175f0f3e
Kerrie Low <klow4@mail.greenriver.edu>
2020-02-14 22:37:01 -0500

    Added model to autoload. Put autoload above session start. Added pet type form. Renamed forms/routes to reflect name, color, type. Attempting to make pet object on result page via function

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 * description:
 */
 
+// require autoload file
+require_once("vendor/autoload.php");
 
 //start a session
 session_start();
@@ -16,12 +18,6 @@ session_start();
 // Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-
-// require autoload file
-require_once("vendor/autoload.php");
-
-require_once('model/validation-functions.php');
-
 
 //instantiate F3
 $f3 = Base::instance();
@@ -32,66 +28,79 @@ $f3->set('DEBUG', 3);
 // array of colors
 $f3->set('colors', array("pink", "green", "blue"));
 
-// pet instantiation
-$pet1 = new Pet("Lorelei", "Tabby", "Cat");
-
 //DEFINE A DEFAULT ROUTE
 $f3->route('GET /', function () {
-    //$view = new Template();
-    //echo $view->render('views/home.html');
     echo "<h1>Hello Pets!</h2>";
 
-    echo "<a href='order'>Order a Pet</a>";
+    echo "<a href='name'>Order a Pet</a>";
 });
 
-$f3->route('GET|POST /order', function($f3) {
+$f3->route('GET|POST /name', function ($f3) {
+//    var_dump($_POST);
 
-    unset($_SESSION['animal']);
+    unset($_SESSION['name']);
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        if(isset($_POST['animal']) && validString($_POST['animal'])){
-            $_SESSION['animal'] = $_POST['animal'];
-            $f3->reroute('/order2');
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (isset($_POST['name']) && validString($_POST['name'])) {
+            $_SESSION['name'] = $_POST['name'];
+            $f3->reroute('/color');
         } else {
-            $f3 -> set("errors['animal']", "please enter an animal.");
+            $f3->set("errors['name']", "please enter a name.");
         }
-
     }
 
     $view = new Template();
-    echo $view->render('views/form1.html');
+    echo $view->render('views/nameForm.html');
 });
 
 
-$f3->route('GET|POST /order2', function($f3) {
+$f3->route('GET|POST /color', function ($f3) {
     //var_dump($_POST);
 
     unset($_SESSION['color']);
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        if(isset($_POST['color']) && validColor($_POST['color'])){
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (isset($_POST['color']) && validColor($_POST['color'])) {
             $_SESSION['color'] = $_POST['color'];
-            $f3->reroute('/results');
+            $f3->reroute('/animal');
         } else {
-            $f3 -> set("errors['color']", "please enter a valid color.");
+            $f3->set("errors['color']", "please enter a valid color.");
         }
     }
 
     $view = new Template();
-    echo $view->render('views/form2.html');
+    echo $view->render('views/colorForm.html');
 });
 
-$f3->route('GET|POST /results', function() {
+$f3->route('GET|POST /animal', function ($f3) {
+//    var_dump($_POST);
 
+    unset($_SESSION['animal']);
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (isset($_POST['animal']) && validString($_POST['animal'])) {
+            $_SESSION['animal'] = $_POST['animal'];
+            $f3->reroute('/results');
+        } else {
+            $f3->set("errors['animal']", "please enter an animal.");
+        }
+    }
+
+    $view = new Template();
+    echo $view->render('views/typeForm.html');
+});
+
+$f3->route('GET|POST /results', function ($f3) {
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        makePet($_SESSION['name'], $_SESSION['color'], $_SESSION['animal']);
+    }
     $view = new Template();
     echo $view->render('views/results.html');
 });
 
 
-
-
-
-$f3->route('GET /@item', function($f3, $params) {
+$f3->route('GET /@item', function ($f3, $params) {
     //var_dump($params);
     $item = $params['item'];
     $petSounds = array("dog" => "Woof", "chicken" => "Cluck cluck", "cat" => "Meow", "fish" => "...", "crow" => "Cawcaw!");
@@ -101,9 +110,6 @@ $f3->route('GET /@item', function($f3, $params) {
         $f3->error(404);
     }
 });
-
-
-
 
 
 //runf3
--- model/validation-functions.php
+++ model/validation-functions.php
@@ -6,11 +6,26 @@
  * @return boolean
  */
 
-function validColor($color) {
+function validColor($color)
+{
     global $f3;
     return in_array($color, $f3->get('colors'));
 }
 
-function validString($string) {
+function validString($string)
+{
     return !empty(trim($string)) && ctype_alpha($string);
+}
+
+function makePet($name, $color, $animal)
+{
+    if (strtolower($animal) == "dog") {
+        $pet1 = new Dog($name, $color, $animal);
+    } else if (strtolower($animal) == "cat") {
+        $pet1 = new Cat($name, $color, $animal);
+    } else {
+        $pet1 = new Pet($name, $color, $animal);
+    }
+
+    $_SESSION['pet1'] = $pet1;
 }
